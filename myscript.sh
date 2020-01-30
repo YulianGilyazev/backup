e4549ec5d977fe86e90d9420878bff416183b2c7
YulianGilyazev <45824886+YulianGilyazev@users.noreply.github.com>
2020-01-30 14:38:27 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -8,23 +8,23 @@ extList=$@
 lol=0
 for ext in $extList
 do
-let lol=lol+1
-if [[ lol -ge 3 ]]
-then
-files=$(find "$myDir")
-for file in $files
-do
-if [ -f "$file" ]
-then
-fileExt="${file##*.}"
-if  [ $fileExt = $ext ]
-then
-let lol=lol+1
-cp /$file ~/$dirName/"$lol"."$ext" >/dev/null 2>&1
-fi
-fi
-done
-fi
+  let lol=lol+1
+  if [[ lol -ge 3 ]]
+  then
+    files=$(find "$myDir")
+    for file in $files
+    do
+      if [ -f "$file" ]
+      then
+        fileExt="${file##*.}"
+        if  [ $fileExt = $ext ]
+        then
+          let lol=lol+1
+          cp /$file ~/$dirName/"$lol"."$ext" >/dev/null 2>&1
+        fi
+      fi
+    done
+  fi
 done
 tar -cf "$archName".tar "$dirName" >/dev/null 2>&1
 rm -R "$dirName" >/dev/null 2>&1
